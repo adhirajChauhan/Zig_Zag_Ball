5e890cbf19636f9bb5bc27ac1eeea0248d93e43e
Adhiraj <adhirajsingh0105@gmail.com>
2020-08-08 23:15:34 +0530

    Adding ball movement and platform spawning

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public GameObject platform;
+
+    Vector3 lastPos;
+    float size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPos = platform.transform.position;
+        size = platform.transform.localScale.x;
+
+        for (int i = 0; i < 6; i++)
+        {
+            spawnX();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnX()
+    {
+        Vector3 pos = lastPos;
+        
+        pos.x += size;
+        lastPos = pos;
+
+        Instantiate(platform, pos, Quaternion.identity);
+    }
+
+    void spawnZ()
+    {
+        Vector3 pos = lastPos;
+        
+        pos.z += size;
+        lastPos = pos;
+
+        Instantiate(platform, pos, Quaternion.identity);
+
+    }
+}
